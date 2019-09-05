8e640dde32864793e5b968e1fbcca024085d2bb0
800414 <800414@seq.org>
2019-09-04 19:08:13 -0700

    finally works!

--- lab903/ball.js
+++ lab903/ball.js
@@ -1,24 +1,20 @@
-
-class Ball{
+class Ball {
   constructor(x, y, dx, dy, id){
-    this.loc = createVector(x, y);
-    this.vel = createVector(dx, dy);
-    this.acc = createVector(0, 1);
-    this.clr = color(random(255), random(255), random(255));
-    this.id = id;
-    this.w = 15
-    if (this.id<0){this.w = 50}
-    //this.width = random(10,80);
+   this.loc = createVector(x, y);
+   this.vel = createVector(dx, dy);
+   this.acc = createVector(0,0);
+   this.clr = color(random(255), random(255), random(255));
+   this.id = id;
 
   }
 
   run(){
-    this.checkEdges();
+    this.checkedges();
     this.update();
     this.render();
   }
 
-  checkEdges(){
+  checkedges(){
     if(this.loc.x < 0){
       this.vel.x = -this.vel.x;
     }
@@ -30,36 +26,41 @@ class Ball{
     }
     if(this.loc.y > height){
       this.vel.y = -this.vel.y;
-      this.loc.y = height-2;
+      this.loc.y = height -2;
     }
-  }
 
+  }
   update(){
     var distToMainBall;
-    if(this.id >= 0){
-      distToMainBall = this.loc.dist(mainBall.loc);
-      if (distToMainBall < 250){
-        // add attraction
-        this.acc = p5.Vector.sub(mainBall.loc, this.loc);
-        this.acc.normalize();
-        this.acc.mult(0.1);
-      }
-      if(distToMainBall<150){
-        this.acc = p5.Vector.sub(mainBall.loc, this.loc);
-        this.acc.normalize();
-        this.acc.mult(0.5);
-      }
-    }
+    if(this.id > 2){
+     distToMainBall = this.loc.dist(mainBall.loc);
 
-    //this.clr = color(random(255), random(255), random(255));
-    this.vel.add(this.acc);
-    this.loc.add(this.vel);
+     if(distToMainBall < 250){
+       //add attraction
+       this.acc = p5.Vector.sub(mainBall.loc, this.loc);
+       this.acc.normalize();
+       this.acc.mult(0.1);
+     }
+     if(distToMainBall < 150){ // add repulsion
+       this.acc = p5.Vector.sub(this.loc, mainBall.loc);
+       this.acc.normalize();
+       this.acc.mult(0.5);
+     }
+
+    }
+    this.vel.limit(10);
+     this.vel.add(this.acc);
+   this.loc.add(this.vel);
+
+
   }
 
   render(){
     fill(this.clr);
-    ellipse(this.loc.x, this.loc.y, this.w, this.w);
-    //fill(this.clr);
-    //rect(this.x, this.y, this.width, this.width);
+    if (this.id <3 ){
+      ellipse (this.loc.x, this.loc.y, 40, 40);
+    }else {
+    ellipse(this.loc.x, this.loc.y, 15, 15);
   }
-}// end ball class
+  }
+}//  +++++++++++++++++++++++++++++++++++  End Ball Class
--- lab903/sketch.js
+++ lab903/sketch.js
@@ -2,39 +2,35 @@
 // Sept 03
 //  attraction
 //  The setup function function is called once when your program begins
-var balls = [];
+
+var balls = []; //declares array
 var mainBall;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5, 7);
-  fill(200, 30, 150);
-    //b1 = new Ball(random(width), random(height), random(-5,5), random(-5,5));
-  // b2 = new Ball(random(width), random(height), random(-5,5), random(-5,5));
-   //b3 = new Ball(random(width), random(height), random(-5,5), random(-5,5));
-  //b4 = new Ball(random(width), random(height), random(-5,5), random(-5,5));
-  loadBalls(100);
- }
+  //background(5, 5, 5, 10);
 
- //  The draw function is called @ 30 fps
- function draw() {
-   background(5, 5, 5, 10);
-  // b1.run();
-   //b2.run();
-  // b3.run();
-   //b4.run();
-   runBalls();
-   }
-   function loadBalls(n){
-     mainBall = new Ball(width/2,height/2,random(-1,1),random(-1,1),-1);
-     for(var i = 0; i < n; i++){
-       balls[i] = new Ball(random(width), random(height), random(-5,5), random(-5,5), i);
+  loadBalls(76);
 
-     }
-   }
+}
+
+
+function draw() {
+background(5,5,5,20);
+
+  runBalls();
+}
+
+function loadBalls(x){
+  mainBall = new Ball(random(width/2), random(height/2), random (-1,1), random(-1,1), 1);
+  for(var i = 0; i < x; i++){
+    balls[i]=new Ball(random(width), random(height), random (-1,1), random(-1,1), i+3);
+  }
+}
+
 function runBalls(){
   mainBall.run();
-  for(var i = 0; i< balls.length; i++){
+  for(var i = 0; i < balls.length; i++){
     balls[i].run();
-  }
-    }
+}
+}
