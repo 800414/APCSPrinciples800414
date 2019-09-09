1df3b09ed2d95028d3401f98ccaf6412289a10e2
800414 <800414@seq.org>
2019-09-09 13:11:47 -0700

    finished test check

--- lab909TestCheck/sketch.js
+++ lab909TestCheck/sketch.js
@@ -13,5 +13,14 @@ function setup() {
 
 //  The draw function is called @ 30 fps
 function draw() {
+  var num = 1;
+  if(num < 4) {
+    console.log("Peace");
+  }
+  console.log("War");
+}
 
+for(var i=14; i>8; i--){
+  i = i-4;
+  console.log(i);
 }
