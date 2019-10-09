cc092af13158c3e6cada8bd14aeb4dc65c590c30
800414 <800414@seq.org>
2019-10-09 08:42:01 -0700

    finished bubble sort

---
+++ LAB1009BUBBLESORT/sketch.js
@@ -0,0 +1,22 @@
+//  Your Name
+// 	Date or version number
+//  This is a comment
+//  The setup function function is called once when your program begins
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+list = [6, 3, 9, 1, 7];
+console.log(list);
+list.sort();
+console.log(list);
+//list.sort();
+//console.log(list);
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+
+}
