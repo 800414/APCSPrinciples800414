debb12c8e813557eedc919bdd323859148f3e41d
800414 <800414@seq.org>
2019-10-10 15:26:34 -0700

    finsihed checkerboard

--- lab1007 Checkerboard/StartCode/CheckerBoard/sketch.js
+++ lab1007 Checkerboard/StartCode/CheckerBoard/sketch.js
@@ -9,45 +9,45 @@
 // Declare squares[] as a global variable
 var squares=[];
 
-// put setup code here
-function setup() {
-  var cnv = createCanvas(800, 800);
-  cnv.position((windowWidth-width)/2, 30);
+//put setup code here
+function setup(){
+  var cnv=createCanvas(800,800);
+  cnv.position((windowWidth-width)/2,30);
   background(235);
   loadSquares();
-  // Create a function that loads the squares into an
-  // array of square objects and call the function
-  for(var i=0; i<squares.length;i++){
+  //traverse array
+  for(var i=0;i<squares.length;i++){
     squares[i].render();
-    }
   }
+}
 
-  function loadSquares() {
-  for(var i=0;i<8;i++){
+function loadSquares(){
+  for (var i=0;i<8;i++){
     for(var j=0;j<8;j++){
       if(i%2===0){
         if(j%2===0){
-          this.clr=color(255,0,0);
-      }
-      if(j%2!==0){
-        this.clr = color(0,255,0);
-      }
-    }
-    if(i%2!==0){
-          if(j%2!==0){
-            this.clr=color(255,0,0);
-          }
-          if(j%2===0){
-            this.clr=color(0,255,0);
-          }
+          var clr=color(255,0,0);
         }
-        squares[j+(8*i)]= new Square(100*j,100*i,100,100, this.clr);
+        if(j%2!==0){
+          var clr=color(0,255,0);
+        }
+      }
+      if(i%2!==0){
+        if(j%2!==0){
+          var clr=color(255,0,0);
+        }
+        if(j%2===0){
+          var clr=color(0,255,0);
+        }
+      }
+      squares[j+(8*i)]= new Square(100*x,100*i,100,100, clr);
+    }
+  }
+}
     //  create a square object and push it into the squares array
 
     //  create a loop that will push 64 squares into an array
     //  calculate the x and y position interms of the loop control variable
-    }
-  }
-}
+
 
   //  traverse array and run each square
--- lab1007 Checkerboard/StartCode/CheckerBoard/square.js
+++ lab1007 Checkerboard/StartCode/CheckerBoard/square.js
@@ -5,21 +5,25 @@
 **  eettlin
 **  Sept. 11, 2018
 */
-class Square {
-//  Add perameters to your constructor function
-constructor(x, y, w, h, clr) {
-  this.loc=createVector(x,y);
-  this.w = w;
-  this.h = h;
-  this.clr = clr;
-}
+class Square{
+  constructor(x,y,w,h,c){
+    this.loc=createVector(x,y);
+    this.w=w;
+    this.h=h;
+    this.clr= c;
+  }
 run(){
+  //main function in class
   this.render();
   this.update();
+
 }
 render(){
+  //creates paddle
   fill(this.clr);
-  rect(this.x, this.y, 100, 100);
+  rect(this.loc.x,this.loc.y,this.w,this.h);
+}
+
 }
 
 //function Sqr(){
