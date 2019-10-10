ca31816fb1e27c322d9bd7553c4c721f17787f82
800414 <800414@seq.org>
2019-10-10 15:29:55 -0700

    finished bubble sort

--- LAB1009BUBBLESORT/sketch.js
+++ LAB1009BUBBLESORT/sketch.js
@@ -7,12 +7,20 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-list = [6, 3, 9, 1, 7];
-list.sort();
+var list = [6, 3, 9, 1, 7];
+function swap(list,a,b){
+  var temp=list[a];
+  list[a]=list[b];
+  list[b]=temp;
+}
+for(var i=0; i<list.length; i++){
+  for (var x=0; x<list.length;x++){
+    if(list[x]>list[x+1]){
+      swap(list,x,x+1);
+    }
+  }
+}
 console.log(list);
 }
 
 //  The draw function is called @ 30 fps
-function draw() {
-
-}
