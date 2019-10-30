8a2cf5c72095b2128b172b9874836fa8a37da382
800414 <800414@seq.org>
2019-10-30 08:34:29 -0700

    comments on comments on comments

--- LAB1023 bars/bar.js
+++ LAB1023 bars/bar.js
@@ -1,19 +1,19 @@
 class Bar {
 
-constructor (x, y, w, h, clr){
+constructor (x, y, w, h, clr){  //determining aspects of bar
 this.loc = createVector(x,y)
 this.w = w;
 this.h = h;
 this.clr = random(color(255));
 }
 
-render(){
+render(){  //how bars appear on screen
 fill(this.clr);
 rect (this.loc.x, this.loc.y, this.w, this.h);
 
 }
 
-run(){
+run(){  //call upon render and update functions to make bar appear properly
   this.render();
   this.update();
 }
--- LAB1023 bars/sketch.js
+++ LAB1023 bars/sketch.js
@@ -2,7 +2,7 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
-var bars=[];
+var bars=[];  //defining global variables
 var barWidth, barHeight;
 
 function setup() {
@@ -11,43 +11,42 @@ function setup() {
   background(255);
   fill(200, 30, 150);
   loadBars();
-  frameRate(4);
+  frameRate(4);  //show animation of each step of sorting bars
   for (var i = 0; i<bars.length; i++){
   bars[i].render();
 }
 }
 
 function draw() {
-  bubbleSort();
+  bubbleSort();  //run sorthing method of bubble
 }
 
-function loadBars(){
+function loadBars(){  //appearance of bars
 
   for (var i = 0; i< 40; i++){
-    barWidth = (width/20);
+    barWidth = (width/32);
     barHeight = Math.floor(random(1, height));
     bars[i]= new Bar(i*barWidth, height-barHeight, barWidth, barHeight);
   }
 }
-
 function  update(){
-  for(var i=0; i<bars.length; i++){
+  for(var i=0; i<bars.length; i++){  //displaying original unsorted bars
     bars[i].set(i);
     }
-    background(255);
+    background(255); //reset background and make sorted bars appear
     for(var i=0; i<bars.length; i++){
       bars[i].run();
     }
 }
-function bubbleSort(){
+function bubbleSort(){  //sort bars by comparing pairs
   for (var x=0; x<bars.length-1;x++){
     if(bars[x].getHeight()>bars[x+1].getHeight()){
-      swap(bars,x,x+1);
+      swap(bars,x,x+1);  ///swap
       update();
     }
   }
 }
-function swap(list,a,b){
+function swap(list,a,b){  //swapping function
   var temp=list[a];
   list[a]=list[b];
   list[b]=temp;
