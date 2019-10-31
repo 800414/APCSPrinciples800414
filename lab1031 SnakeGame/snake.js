adbbb6fca8cffde47147901bcf5d61398298142e
800414 <800414@seq.org>
2019-10-31 08:47:43 -0700

    started snake game kinda

---
+++ lab1031 SnakeGame/snake.js
@@ -0,0 +1,21 @@
+class Snake {
+  constructor(x, y, clr){
+    this.loc = createVector(x, y);
+    this.clr = random(color(255));
+  }
+  render(){  //how snake appear on screen
+  fill(this.clr);
+  rect (this.loc.x, this.loc.y, this.w, this.h);
+
+  }
+
+  run(){  //call upon render and update functions to make snake appear properly
+    this.render();
+    this.update();
+  }
+
+  update(){
+
+  }
+
+}
