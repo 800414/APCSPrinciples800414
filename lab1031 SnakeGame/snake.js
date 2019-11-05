fc3efceb024fb7103e83e70913e7da59c3d757cc
800414 <800414@seq.org>
2019-11-05 08:48:01 -0800

    snek move

--- lab1031 SnakeGame/snake.js
+++ lab1031 SnakeGame/snake.js
@@ -1,12 +1,9 @@
 class Snake {
-  constructor(x, y, clr){
+  constructor(x, y, w, h){
     this.loc = createVector(x, y);
-    this.clr = random(color(255));
-  }
-  render(){  //how snake appear on screen
-  fill(this.clr);
-  rect (this.loc.x, this.loc.y, this.w, this.h);
-
+    //this.clr = random(color(255));
+    this.w = w;
+    this.h = h;
   }
 
   run(){  //call upon render and update functions to make snake appear properly
@@ -15,11 +12,33 @@ class Snake {
   }
 
   update(){
-
+    this.keyReleased();
   }
+
+  render(){  //how snake appear on screen
+  fill(143,47,68);
+  rect (this.loc.x, this.loc.y, this.w, this.h);
+  }
+
 keyReleased(){
   if(keyCode === UP_ARROW){
     this.loc.y = this.loc.y - this.h
   }
+  if(keyCode === DOWN_ARROW){
+    this.loc.y = this.loc.y + this.h
+  }
+  if(keyCode === LEFT_ARROW){
+    this.loc.x = this.loc.x - this.w
+  }
+  if(keyCode === RIGHT_ARROW){
+    this.loc.x = this.loc.x + this.w
+  }
 }
+//checkEdges(){
+//  if(this.loc.x > 800 ||
+  //  this.loc.x < 0 ||
+  //  this.loc.y > 800 ||
+  //  this. loc.y < 0){
+  //    clear();
+  //  }
 }
