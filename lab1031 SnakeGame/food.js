be9d89494d6f10a4bec8eeb15bff3b7fd5be1ea5
800414 <800414@seq.org>
2019-11-07 09:48:55 -0800

    i mean i tried

--- lab1031 SnakeGame/food.js
+++ lab1031 SnakeGame/food.js
@@ -9,14 +9,16 @@ class Food {
     this.render();
   }
   update(){
-  //  if(this.food.x > this.snake.x &&
-    //   this.food.x  < this.snake.x + snake.w &&
-    //   this.food.y > paddle.food.y &&
-    //   this.food.y  < paddle.food.y + paddle.h)
-    //   {
-      //   return true;
-      //   this.clr = color(random(255), random(255), random(255));
-        //    }
+    if(this.food.x + 15 > snake.x &&
+       this.food.x - 15 < snake.x + snake.w &&
+       this.food.y + 15 > snake.y &&
+       this.food.y - 15 < snake.y + snake.h)
+       {
+        return true;
+        this.clr = color(random(255), random(255), random(255));
+        this.food.x = random(100,700);
+        this.food.y = random(100, 700);
+            }
   }
   render(){
     fill(66, 230, 245);
